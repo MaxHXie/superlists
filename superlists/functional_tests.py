db68938ee4fd97fafcfd4ffeb4fef7e626194879
Max Xie <maxhxie@gmail.com>
2018-06-16 00:25:00 +0200

    Implemented unittesting and wrote the first test using selenium.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,30 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #Edith has heard about a cool new online to-do app. She goes
+        #to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        #She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+    #Tests are organized into classes, which inherit from unittest.TestCase
+
+    #The main body of the test is in a method called test_can_start_a_list_and_retrieve_it_later. Any method whose name start starts test is a test method, and will be run by the test runner. You can have more than one test_ method per class. Nice descriptive names for our test methods are a good idea too.
+
+    #setUp and tearDown are special methods which get run before and after each testselfself.
+    #tearDown will run even if there is an error in the test. So, no more firefox windows lying around!
