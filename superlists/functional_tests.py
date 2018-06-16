4ad01060687900dde014e0305578c8af8133fb29
Max Xie <maxhxie@gmail.com>
2018-06-16 02:35:15 +0200

    Functoinal test now checks we can input a to-do item.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,23 +17,47 @@ class NewVisitorTest(unittest.TestCase):
 
         #She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-        # She is invited to enter a to-do item straight away
+        #She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('pladeholder'),
+            'Enter a to-do item'
+        )
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+        #She types "Buy peacock feathers" into a text box (Edith's hobby
+        #is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-    #Tests are organized into classes, which inherit from unittest.TestCase
+        #When she hits enter, the page updates, and now the page lists
+        #"1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-    #The main body of the test is in a method called test_can_start_a_list_and_retrieve_it_later. Any method whose name start starts test is a test method, and will be run by the test runner. You can have more than one test_ method per class. Nice descriptive names for our test methods are a good idea too.
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
-    #setUp and tearDown are special methods which get run before and after each testselfself.
-    #tearDown will run even if there is an error in the test. So, no more firefox windows lying around!
+        #There is still a text box inviting her to add another item. She
+        #enters "Use peacock feathers to make a fly" (Edith is very
+        #methodological)
+        self.fail('Finish the test!')
 
-# Man vet inte om de gjort fel, fuskbygge etc.
+        #The page updates again, and now shows both items on her list
 
-# Work progress:
-# 1. Write a functional test
-# 2. Write unit test
-# 3. Write application code and solve the unittests
-# 4. Rerun functional tests to see if they pass
+
+        # 1. We are using several of the methods that Selenium provides to examine web pages
+        #find_element_by_tag_name, find_element_by_id and find_elements_by_tag_name (notice the extra s,
+        #which means it will return several elements rather than just one.)
+
+        #2. We also use send_keys, which is Selenium's way of typing into input elements.
+
+        #3. The Keys class (don't forget ot import it) lets us send special keys like Enter?
+
+        #4. When we hit Enter, the page will refresh. The time.sleep is there to make sure the browser
+        #has finished loading before we make any assertiong about the new page. This is called an "explicit way"
+        #(a very simple one; we'll improve it in Chapter 6)
