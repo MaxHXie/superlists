3af62288afc4eeccf674db79ef616cd2d40f1abc
Max Xie <maxhxie@gmail.com>
2018-06-16 18:34:26 +0200

    Added functional test, testing multiple items in to-do list.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -43,22 +43,25 @@ class NewVisitorTest(unittest.TestCase):
         #There is still a text box inviting her to add another item. She
         #enters "Use peacock feathers to make a fly" (Edith is very
         #methodological)
-        self.fail('Finish the test!')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         #The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
-        # 1. We are using several of the methods that Selenium provides to examine web pages
-        #find_element_by_tag_name, find_element_by_id and find_elements_by_tag_name (notice the extra s,
-        #which means it will return several elements rather than just one.)
+        #Edith wonders whether the site will remember her list. Then she sees
+        #that the site has generated a unique URL for her -- there is some
+        #explanatory text to that effect.
 
-        #2. We also use send_keys, which is Selenium's way of typing into input elements.
+        self.fail('Finish the test!')
 
-        #3. The Keys class (don't forget ot import it) lets us send special keys like Enter?
-
-        #4. When we hit Enter, the page will refresh. The time.sleep is there to make sure the browser
-        #has finished loading before we make any assertiong about the new page. This is called an "explicit way"
-        #(a very simple one; we'll improve it in Chapter 6)
-
+        #She visits that URL - her to-do list is still there.
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
