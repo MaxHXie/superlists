cf64b73026e46715f07389cc67efc1de6ee16dab
Max Xie <maxhxie@gmail.com>
2018-06-16 21:49:32 +0200

    Model for list Items and associated migration

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -2,6 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from lists.models import Item
 
 from lists.views import home_page
 
@@ -21,27 +22,19 @@ class HomePageTest(TestCase):
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
-'''
-Red/Green/Refactor and Triangulation
+    def test_saving_and_retrieving(self):
+            first_item = Item()
+            first_item.text = 'The first (ever) list item'
+            first_item.save()
 
-The unit-test/code cycle is sometimes taught as Red, Green, Refactor
+            second_item = Item()
+            second_item.text = 'Item the second'
+            second_item.save()
 
-    - Start by writing a unit test which fails (Red).
-    - Write the simplest possible code to get it to pass (Green), even if that means cheating.
-    - Refactor to get to better code that makes more sense.
+            saved_items = Item.objects.all()
+            self.assertEqual(saved_items.count(), 2)
 
-So what do we do during the Refactoring stage? What justifies moving from an implementation
-where we "cheat" to one we're happy with?
-
-One methodology is eliminate duplication: if your test uses a magic constant (like the
-"1:" in front of our list item), and your application code also uses it, that counts as a
-duplication, so it justifies refactoring. Removing the magic constant from the application code
-usually means you have to stop cheating.
-
-I find that leaves things a little too vague, so I usually like to use a second technique, which is
-called triangulation: if your tests let you get away with writing "cheating" code that you're
-not happy with, like returning a magic constant, write another test that forces you to write
-some better code. That's what we're doing when we extend the FT to check that we get "2:"
-when inputting a second list item.
-
-'''
+            first_saved_item = saved_items[0]
+            second_saved_item = saved_items[1]
+            self.assertEqual(first_saved_item, "The first (ever) list item")
+            self.assertEqual(second_saved_item, "Item the second")
